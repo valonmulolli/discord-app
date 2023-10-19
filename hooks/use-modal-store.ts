1bab489be8110bce490df169f5d768914126d136
valonmulolli <valonmulolli@gmail.com>
2023-10-19 16:46:49 +0200

    edit server settings

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 
-export type ModalType = 'createServer' | 'invite';
+export type ModalType = 'createServer' | 'invite' | "editServer";
 
 interface ModelData {
 	server?: string;
