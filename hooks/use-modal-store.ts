26768d1cdbf8d3809555faeb3f3ad3a43ef3d6ce
valonmulolli <valonmulolli@gmail.com>
2023-10-23 12:44:42 +0200

    delete and leave modal

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,7 +1,7 @@
 import { Server } from 'http';
 import { create } from 'zustand';
 
-export type ModalType = 'createServer' | 'invite' | "editServer" | "members" | "createChannel";
+export type ModalType = 'createServer' | 'invite' | "editServer" | "members" | "createChannel" | "leaveServer" | "deleteServer";
 
 interface ModalData {
 	server?: Server;
